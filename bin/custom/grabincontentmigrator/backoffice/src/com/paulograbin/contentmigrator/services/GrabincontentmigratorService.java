cc0ab18a763a0ce1c6f6915cac36eb102b7fc643
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-03-03 09:38:38 -0300

    Introduce a new extension to hold content migrator code

---
+++ bin/custom/grabincontentmigrator/backoffice/src/com/paulograbin/contentmigrator/services/GrabincontentmigratorService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package com.paulograbin.contentmigrator.services;
+
+/**
+ * Hello World GrabincontentmigratorService
+ */
+public class GrabincontentmigratorService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
