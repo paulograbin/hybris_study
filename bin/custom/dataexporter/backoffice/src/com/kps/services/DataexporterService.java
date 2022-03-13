6d21ab3f84f24b3ec014f76b9a8fa2f0fa5af9d7
Diego Reidel <diego.reideldasilva@kps.com>
2022-03-12 21:10:46 -0300

    Introducing a new extension

---
+++ bin/custom/dataexporter/backoffice/src/com/kps/services/DataexporterService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package com.kps.services;
+
+/**
+ * Hello World DataexporterService
+ */
+public class DataexporterService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
---
+++ bin/custom/dataexporter/src/com/kps/constants/DataexporterConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package com.kps.constants;
+
+/**
+ * Global class for all Ybackoffice constants. You can add global constants for your extension into this class.
+ */
+public final class DataexporterConstants extends GeneratedDataexporterConstants
+{
+	public static final String EXTENSIONNAME = "dataexporter";
+
+	private DataexporterConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
