55b50c4602c8c833e1d5884a850bad9fe20e7b42
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-03-16 15:17:57 -0300

    Include more types to Page generator

--- bin/custom/dataexporter/src/com/kps/dataexporter/impex/impl/AbstractPageImpexGenerator.java
+++ bin/custom/dataexporter/src/com/kps/dataexporter/impex/impl/AbstractPageImpexGenerator.java
@@ -2,17 +2,31 @@ package com.kps.dataexporter.impex.impl;
 
 import com.kps.dataexporter.impex.ImpexGenerator;
 import com.kps.dataexporter.impex.ImpexHeaderGenerationService;
+import de.hybris.platform.acceleratorcms.model.components.BreadcrumbComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.CategoryNavigationComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.FooterNavigationComponentModel;
 import de.hybris.platform.acceleratorcms.model.components.JspIncludeComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.LogoComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.MiniCartComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.NavigationComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.SearchBoxComponentModel;
 import de.hybris.platform.acceleratorcms.model.components.SimpleBannerComponentModel;
+import de.hybris.platform.acceleratorcms.model.components.SimpleResponsiveBannerComponentModel;
 import de.hybris.platform.acceleratorservices.model.cms2.pages.DocumentPageModel;
 import de.hybris.platform.acceleratorservices.model.cms2.pages.EmailPageModel;
 import de.hybris.platform.cms2.model.contents.components.AbstractCMSComponentModel;
+import de.hybris.platform.cms2.model.contents.components.CMSLinkComponentModel;
 import de.hybris.platform.cms2.model.contents.components.CMSParagraphComponentModel;
+import de.hybris.platform.cms2.model.contents.components.SimpleCMSComponentModel;
 import de.hybris.platform.cms2.model.contents.contentslot.ContentSlotModel;
 import de.hybris.platform.cms2.model.pages.*;
 import de.hybris.platform.cms2.model.relations.ContentSlotForPageModel;
 import de.hybris.platform.cms2.model.relations.ContentSlotForTemplateModel;
+import de.hybris.platform.cms2lib.model.components.AbstractBannerComponentModel;
+import de.hybris.platform.cms2lib.model.components.ProductCarouselComponentModel;
 import de.hybris.platform.core.PK;
+import de.hybris.platform.core.model.media.MediaModel;
+import de.hybris.platform.personalizationcms.model.CxCmsComponentContainerModel;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
@@ -36,13 +50,28 @@ public class AbstractPageImpexGenerator extends AbstractImpexGenerator<AbstractP
         typeList.add(EmailPageModel._TYPECODE);
         typeList.add(DocumentPageModel._TYPECODE);
 
-        typeList.add(PageTemplateModel._TYPECODE);
-        typeList.add(ContentSlotModel._TYPECODE);
-        typeList.add(ContentSlotForTemplateModel._TYPECODE);
-        typeList.add(ContentSlotForPageModel._TYPECODE);
-        typeList.add(SimpleBannerComponentModel._TYPECODE);
-        typeList.add(JspIncludeComponentModel._TYPECODE);
+        typeList.add(AbstractBannerComponentModel._TYPECODE);
+        typeList.add(AbstractCMSComponentModel._TYPECODE);
+        typeList.add(BreadcrumbComponentModel._TYPECODE);
+        typeList.add(CMSLinkComponentModel._TYPECODE);
         typeList.add(CMSParagraphComponentModel._TYPECODE);
+        typeList.add(CategoryNavigationComponentModel._TYPECODE);
+        typeList.add(ContentSlotForPageModel._TYPECODE);
+        typeList.add(ContentSlotForTemplateModel._TYPECODE);
+        typeList.add(ContentSlotModel._TYPECODE);
+        typeList.add(CxCmsComponentContainerModel._TYPECODE);
+        typeList.add(FooterNavigationComponentModel._TYPECODE);
+        typeList.add(JspIncludeComponentModel._TYPECODE);
+        typeList.add(LogoComponentModel._TYPECODE);
+        typeList.add(MediaModel._TYPECODE);
+        typeList.add(MiniCartComponentModel._TYPECODE);
+        typeList.add(NavigationComponentModel._TYPECODE);
+        typeList.add(PageTemplateModel._TYPECODE);
+        typeList.add(ProductCarouselComponentModel._TYPECODE);
+        typeList.add(SearchBoxComponentModel._TYPECODE);
+        typeList.add(SimpleBannerComponentModel._TYPECODE);
+        typeList.add(SimpleCMSComponentModel._TYPECODE);
+        typeList.add(SimpleResponsiveBannerComponentModel._TYPECODE);
 
         return typeList;
     }
